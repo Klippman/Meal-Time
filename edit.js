d5c536bdb2049efc16e9d52e82e0f208bd2bf9ba
Devon.Klipp <devonklipp@gmail.com>
2019-02-26 20:30:38 -0500

    refactored functions

--- edit.js
+++ edit.js
@@ -1,56 +1,11 @@
-let ingredients = []
+const ingredients = getSavedIngredients()
 
 const ingredientFilters = {
     searchText: '',
     hideCompleted: false
 }
 
-const ingredientsJSON = localStorage.getItem('ingredients')
-    if (ingredientsJSON !== null) {
-        ingredients = JSON.parse(ingredientsJSON)
-    } 
-
-const renderIngredients = (ingredients, ingredientFilters) => {
-    const filteredIngredients = ingredients.filter((ingredient) => {
-        return ingredient.name.toLowerCase().includes(ingredientFilters.searchText.toLowerCase())
-    })
-
-    document.querySelector('#ingredients').innerHTML = ''
-
-    filteredIngredients.map(ingredient => {
-        // Create a container for the ingredients
-        const ingredientEl = document.createElement('p')
-        const ingredientName = document.createElement('label')
-        const checkBox = document.createElement('input')
-        const removeEl = document.createElement('button')
-    
-        document.querySelector('#ingredients').appendChild(ingredientEl)
-
-        if (ingredient.name.length > 0) {
-            ingredientName.textContent = ingredient.name
-        } else {
-            ingredientName.textContent = 'Unnamed Ingredient'
-        }
-        
-    
-        checkBox.checked = ingredient.completed
-        checkBox.setAttribute('type', 'checkbox')
-        removeEl.innerHTML = 'Remove'
-        removeEl.classList.add('remove_button')
-
-        // Add all elements to the ingredientEl as you create them
-        ingredientEl.appendChild(checkBox)
-        ingredientEl.appendChild(ingredientName)
-        ingredientEl.appendChild(removeEl)
-    })
-}
-
 renderIngredients(ingredients, ingredientFilters)
-
-document.querySelector('input').addEventListener('input', (e) => {
-    ingredientFilters.searchText = e.target.value
-    renderIngredients(ingredients, ingredientFilters)
-})
 
 // Add a new ingredient
 document.querySelector('#ingredient_form').addEventListener('submit', (e) => {
@@ -60,7 +15,7 @@ document.querySelector('#ingredient_form').addEventListener('submit', (e) => {
         name: ingredient,
         completed: false
     })
-    localStorage.setItem('ingredients', JSON.stringify(ingredients))
+    saveIngredients(ingredients)
     renderIngredients(ingredients, ingredientFilters)
     e.target.elements.addIngredient.value = ''
 })
---
+++ functions.js
@@ -0,0 +1,56 @@
+// Read existing notes from localStorage
+const getSavedIngredients = function () {
+    const ingredientsJSON = localStorage.getItem('ingredients')
+        if (ingredientsJSON !== null) {
+            return JSON.parse(ingredientsJSON)
+        } else {
+            return []
+        }
+}
+
+// Save ingredients to localStorage
+const saveIngredients = function (ingredients) {
+    localStorage.setItem('ingredients', JSON.stringify(ingredients))
+}
+
+// Generate DOM structure for ingredient
+const generateIngredientDOM = function (ingredient) {
+    const ingredientName = document.createElement('label')
+
+    if (ingredient.name.length > 0) {
+        ingredientName.textContent = ingredient.name
+    } else {
+        ingredientName.textContent = 'Unnamed Ingredient'
+    }
+
+    return ingredientName
+}
+
+// Render application ingredients
+const renderIngredients = (ingredients, ingredientFilters) => {
+    const filteredIngredients = ingredients.filter((ingredient) => {
+        return ingredient.name.toLowerCase().includes(ingredientFilters.searchText.toLowerCase())
+    })
+
+    document.querySelector('#ingredients').innerHTML = ''
+
+    filteredIngredients.map(ingredient => {
+        // Create a container for the ingredients
+        const ingredientEl = document.createElement('p')
+        const ingredientName = generateIngredientDOM(ingredient)
+        const checkBox = document.createElement('input')
+        const removeEl = document.createElement('button')
+    
+        document.querySelector('#ingredients').appendChild(ingredientEl)
+    
+        checkBox.checked = ingredient.completed
+        checkBox.setAttribute('type', 'checkbox')
+        removeEl.innerHTML = 'Remove'
+        removeEl.classList.add('remove_button')
+
+        // Add all elements to the ingredientEl as you create them
+        ingredientEl.appendChild(checkBox)
+        ingredientEl.appendChild(ingredientName)
+        ingredientEl.appendChild(removeEl)
+    })
+}
